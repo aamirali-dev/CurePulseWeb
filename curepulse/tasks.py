440eb4e0532a47a613936f7d83f262d839be4a70
MAasifM <maasifmustafa@gmail.com>
2023-10-19 12:27:03 +0500

    Bugs

--- curepulse/tasks.py
+++ curepulse/tasks.py
@@ -5,19 +5,18 @@ This file contains the celery task that updates the database with the latest dat
 from datetime import date as Date
 from celery import shared_task
 from CurePulse.controller import Controller
+import traceback
 
 @shared_task
 def update_database():
 	"""_summary_
  	This function is used to update the database with the latest data from the CurePulse API.
 	"""
-	today_date = Date.today()
 	try:
-		date = str(today_date)
+		date = str(Date(2023,9,14))
 		controller = Controller()
 		controller.execute(date)
-	except Exception as e:
-		print(e)
-		pass 
-
+	except Exception:
+		traceback.print_exc()
+  
 update_database()
