c660d44db4eb6bbadc2fdad8d1124a5c95a04c9e
MAasifM <maasifmustafa@gmail.com>
2023-10-25 11:19:55 +0500

    Good Clustering

--- curepulse/CurePulse/DataExporter/CSVExporter.py
+++ curepulse/CurePulse/DataExporter/CSVExporter.py
@@ -4,7 +4,7 @@ import numpy as np
 class CSVExporter:
     def __init__(self) -> None:
         self.filename = 'stats.csv'
-        self.list_scores = ['Client_Tone_Scores', 'Client_Text_Scores', 'Agent_Tone_Scores', 'Agent_Text_Scores']
+        self.list_scores = ['Agent_Tone_Scores', 'Client_Tone_Scores', 'Client_Text_Scores', 'Agent_Text_Scores']
         self.ratings = [1, 2, 3, 3.5, 4, 4.5, 5]
         self.current_score = None
 
@@ -32,16 +32,30 @@ class CSVExporter:
             column_name = f'{score}_Star_Rating'
             value_counts = results[column_name].value_counts().to_frame()
             value_counts.columns = [f'{score}_Count']
-            results[f'{score}_Single'] = results[score].apply(self.transform_list_to_single_value)
+            # results[f'{score}_Single'] = results[score].apply(self.transform_list_to_single_value)
             results[score] = results[score].apply(self.transform_dict_to_list)
-            rating_range = results.groupby(by=[column_name])[score].agg(['min', 'max'])
-            rating_range.columns = [f'{score}_Min', f'{score}_Max']
-            rating_range[[f'{score}_Min_N', f'{score}_Min_Ne', f'{score}_Min_P']] = pd.DataFrame(rating_range[f'{score}_Min'].tolist(), index=rating_range.index)
-            rating_range[[f'{score}_Max_N', f'{score}_Max_Ne', f'{score}_Max_P']] = pd.DataFrame(rating_range[f'{score}_Max'].tolist(), index=rating_range.index)
-            rating_range = rating_range.drop(columns=[f'{score}_Min', f'{score}_Max'])
+            rating_range = results.groupby(by=[column_name]).apply(self.split_array_to_columns, array_column_name=score, column_name=column_name)
+            # print(rating_range)
+            # print(rating_range.columns)
+            # dadsa
+            rating_range = rating_range.reset_index(level=0, drop=True)
+            # print("Rating Range Columns")
+            # print(rating_range.columns)
+            # print("Value Counts Columns")
+            # print(value_counts.columns)
+            # print("Rating Range Index")
+            # print(rating_range.index)
+            # print("Value Counts Index")
+            # print(value_counts.index)
+            # print("Rating Range")
+            # print(rating_range)
+            # print("Value Counts")
+            # print(value_counts)
             df = df.join(value_counts)
             df = df.join(rating_range)
-        
+            df = df.fillna(0.0)
+        print(df.T)
+        print(df.columns)
         cols = ['Agent_Accent_Score', 'Agent_Langauge_Score_Percentage']
         score = cols[0]
         column_name = f'{score}_Star_Rating'
@@ -80,6 +94,27 @@ class CSVExporter:
         df = df.join(rating_range)
         df = df.fillna(0.0).applymap(lambda x: self.round_array(x) if isinstance(x, list) else round(x, 5))
         df.to_csv(self.filename)
+    
+    def split_array_to_columns(self, grouped_df, column_name, array_column_name):
+        def split_array(row):
+            return row[array_column_name][0], row[array_column_name][1], row[array_column_name][2]
+        grouped_df[f'{array_column_name}_Negative'], grouped_df[f'{array_column_name}_Neutral'], grouped_df[f'{array_column_name}_Positive'] = zip(*grouped_df.apply(split_array, axis=1))
+        max_values = grouped_df.groupby(column_name)[[f'{array_column_name}_Negative', f'{array_column_name}_Neutral', f'{array_column_name}_Positive']].max()
+        min_values = grouped_df.groupby(column_name)[[f'{array_column_name}_Negative', f'{array_column_name}_Neutral', f'{array_column_name}_Positive']].min()
+        max_values.reset_index(inplace=True)
+        min_values.reset_index(inplace=True)
+        result = pd.merge(max_values, min_values, on=column_name, suffixes=('_max', '_min'))
+        result = result.set_index(column_name)
+        new_column_order = [f'{array_column_name}_Negative_min', f'{array_column_name}_Negative_max', f'{array_column_name}_Neutral_min', f'{array_column_name}_Neutral_max', f'{array_column_name}_Positive_min', f'{array_column_name}_Positive_max']
+        result.reset_index(inplace=True)
+        result.set_index(column_name, inplace=True)
+        result = result[new_column_order]
+        # print(result.columns)
+        # print(result.T)
+        # print(result.index)
+        # print(result.shape)
+        # asds
+        return result
     
     def round_array(self, arr):
         return [round(x, 5) for x in arr]
